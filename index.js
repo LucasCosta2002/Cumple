c9af18c510ad2b90b3281ae3bbb06d9ce7642445
Lucas <lukas19costamagna@gmail.com>
2024-02-10 16:03:49 -0300

    final

--- index.js
+++ index.js
@@ -67,46 +67,6 @@ var swiper = new Swiper(".swiper", {
   },
 });
 
-
-// (function () {
-//     const second = 1000,
-//           minute = second * 60,
-//           hour = minute * 60,
-//           day = hour * 24;
-  
-//     let today = new Date(),
-//         dd = String(today.getDate()).padStart(2, "0"),
-//         mm = String(today.getMonth() + 1).padStart(2, "0"),
-//         yyyy = today.getFullYear(),
-//         nextYear = yyyy + 1,
-//         dayMonth = "09/30/",
-//         birthday = dayMonth + yyyy;
-    
-//     today = mm + "/" + dd + "/" + yyyy;
-//     if (today > birthday) {
-//       birthday = dayMonth + nextYear;
-//     }
-    
-//     const countDown = new Date(birthday).getTime(),
-//         x = setInterval(function() {    
-  
-//           const now = new Date().getTime(),
-//                 distance = countDown - now;
-  
-//           document.getElementById("days").innerText = Math.floor(distance / (day)),
-//             document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-//             document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-//             document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-  
-//           if (distance < 0) {
-//             document.getElementById("headline").innerText = "It's my birthday!";
-//             document.getElementById("countdown").style.display = "none";
-//             document.getElementById("content").style.display = "block";
-//             clearInterval(x);
-//           }
-//         }, 0)
-// }());
-
 (function () {
 	window.onload = () => {
 		const obj = document.querySelector("#gallery");
@@ -270,9 +230,13 @@ const fechaObjetivo = new Date('July 3, 2024 00:00:00').getTime();
 }
 let intervalo = setInterval(actualizarContador, 1000);
 
-
-const formPiedra = document.getElementById('piedra');
-
-formPiedra.addEventListener('submit', (e) => {
-    e.preventDefault();
-})
+var swiperCube = new Swiper(".mySwiperCube", {
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
+    },
+});
